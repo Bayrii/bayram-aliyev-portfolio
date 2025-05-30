9f7199663754b186de82107e77ade680c1e5ce2a
Bayram Aliyev <aliyevbayram08@gmail.com>
2025-05-30 21:06:28 +0400

    Improve Contact Me form

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -5,29 +5,22 @@ st.header("📧 Contact Me")
 st.write(
     "Have a question, opportunity, or just want to connect? Fill out the form below and I’ll get back to you as soon as possible.")
 
-if "email_sent" not in st.session_state:
-    st.session_state["email_sent"] = False
 
-if not st.session_state["email_sent"]:
-    with st.form(key="email_form", clear_on_submit=True):
-        user_email = st.text_input("Your Email Address", placeholder="you@example.com")
-        raw_message = st.text_area("Your Message", placeholder="Write your message here...")
+with st.form(key="email_form", clear_on_submit=True):
+    user_email = st.text_input("Your Email Address", placeholder="you@example.com")
+    raw_message = st.text_area("Your Message", placeholder="Write your message here...")
 
-        submit_button = st.form_submit_button("Send Message")
+    submit_button = st.form_submit_button("Send Message")
 
-        if submit_button:
-            message = f"""\
+    if submit_button:
+        message = f"""\
 Subject: Portfolio Contact Me - {user_email}
 
-        From: {user_email}
-        To: Portfolio Inbox
+From: {user_email}
+To: Portfolio Inbox
 
-        {raw_message}
-        """
-            send_email(message)
-            st.session_state["email_sent"] = True
-            st.success("✅ Your message has been sent successfully!")
-else:
-    st.info("✅ Your message has been sent successfully!")
-    if st.button("Send another message"):
-        st.session_state["email_sent"] = False
+{raw_message}
+    """
+        send_email(message)
+        st.session_state["email_sent"] = True
+        st.success("✅ Your message has been sent successfully!")
